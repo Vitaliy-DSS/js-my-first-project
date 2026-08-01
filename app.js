e87cbb24deb189622f3553673678d99264238b80
Vitaliy Niko <solutionsbypvv@gmail.com>
2026-08-01 19:46:20 +0300

    Add button message

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello from VS Code!");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Git makes changes easy to track!");
+});
